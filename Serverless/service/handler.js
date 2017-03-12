ada11e948cc4fe597ee8854d24501c779c2baa87
John Kim <joimkim@gmail.com>
2017-03-12 03:20:06 -0700

    back to mock data

--- Serverless/service/handler.js
+++ Serverless/service/handler.js
@@ -292,7 +292,7 @@ module.exports.restaurantDetails = (event, context, callback) => {
   .catch(e => callback(e));
 }
 
-var makeMealTypes = (num) => ['Dessert', 'Dinner', 'Coffee', 'Lunch', 'Breakfast'].slice(0, num || 6);
+var makeMealTypes = (num = 6) => ['Dessert', 'Dinner', 'Coffee', 'Lunch', 'Breakfast'].slice(0, num);
 module.exports.fetchRestaurantItinerary = (event, context, callback) => {
   var vars = event || {};
   if (typeof vars === 'string') {
