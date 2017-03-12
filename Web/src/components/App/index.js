62c4e488969fd85fcfead5f85b55facc1642137b
John Kim <joimkim@gmail.com>
2017-03-11 16:12:12 -0800

    merge

--- Web/src/components/App/index.js
+++ Web/src/components/App/index.js
@@ -1,26 +1,20 @@
 // src/components/App/index.js
 import React, { Component } from 'react';
-import classnames from 'classnames';
 import Homepage from '../Homepage';
 import logo from './logo.svg';
 import './style.css';
 
 class App extends Component {
-  static propTypes = {}
-  static defaultProps = {}
-  state = {}
 
   render() {
     const webpage = this.props.children ? React.cloneElement(this.props.children,
         this.props) : <Homepage {... this.props} />;
 
-    const { className, ...props } = this.props;
     return (
-      <div className={classnames('App', className)} {...props}>
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
+      <div className="App">
+          <div className="Search">
+            hello
+          </div>
           { webpage }
       </div>
     );
