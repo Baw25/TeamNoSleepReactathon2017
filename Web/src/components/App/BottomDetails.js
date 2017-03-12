aead7c1ea73727363b04f846cb41c7aebfe9201e
Blake Wills <blake.wills525@gmail.com>
2017-03-11 17:29:28 -0800

    Add descriptions for restaurant pics

--- Web/src/components/App/BottomDetails.js
+++ Web/src/components/App/BottomDetails.js
@@ -1,13 +1,14 @@
 import React, {Component} from 'react';
+import StarRatingComponent from 'react-star-rating-component';
 
 // import Booking from "./Booking"
 import './BottomDetails.css'
 
 const mapper = [
-  ['1',"Bar Agricole","Inventory: 40","Price: $20"],
-  ['2',"Serpentine", "Inventory: 56","Price: $25"],
-  ['3',"Piccino", "Inventory: 25","Price: $30"],
-  ['4',"Padrecito","Inventory: 47","Price: $20"]
+  ['1',"Bar Agricole","Californian | SOMA","Booked 32 times"],
+  ['2',"Serpentine", "Contemporary American","Booked 23 times"],
+  ['3',"Piccino", "Californian","Booked 46 times"],
+  ['4',"Padrecito","Mexican | Cole Valley","Booked 54 times"]
 ];
 
   // ['5',"Limon Rotisserie","Inventory: 6","Price: $25"]
@@ -16,10 +17,17 @@ const mapper = [
 class BottomDetails extends Component {
   constructor(props){
     super(props);
-    this.state = {};
+    this.state = {
+      rating: 4
+    };
+  }
+
+  onStarClick(nextValue, prevValue, name) {
+    this.setState({rating: nextValue});
   }
 
   render(){
+    const { rating } = this.state;
     return (
       <div className="OrderBackground">
         <div className="OrderContainers">
@@ -28,6 +36,13 @@ class BottomDetails extends Component {
               <div>
                 <div className={`OrderItem${pic[0]} orders`}></div>
                 <strong><p className='pic-descriptions'>{pic[1]}</p></strong>
+                <StarRatingComponent 
+                    name="rate1" 
+                    starCount={5}
+                    value={rating}
+                    onStarClick={this.onStarClick.bind(this)} />
+                <p className='food-type'>{pic[2]}</p>
+                <p className='book-type'>{pic[3]}</p>
               </div>
             );
           })}
