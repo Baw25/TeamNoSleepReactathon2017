b06dc3dd7ef3b656162852930a7e1cbd6789a2c2
John Kim <joimkim@gmail.com>
2017-03-11 21:02:25 -0800

    search bar

--- Web/src/components/App/index.js
+++ Web/src/components/App/index.js
@@ -1,11 +1,31 @@
 // src/components/App/index.js
 import React, { Component } from 'react';
+import DatePicker from'react-datepicker';
+import moment from'moment'
 import Homepage from '../Homepage';
 import Header from '../Header';
+import Form from '../Form';
 import logo from './logo.svg';
 import './style.css';
+import './react-datepicker-cssmodules.css';
 
 class App extends Component {
+  
+  constructor() {
+    super();
+  
+    this.state = {
+      date: moment()
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(date) {
+    this.setState({
+      date
+    });
+  }
 
   render() {
     const webpage = this.props.children ? React.cloneElement(this.props.children,
@@ -13,9 +33,30 @@ class App extends Component {
 
     return (
       <div className="App">
-          <Header />
           <div className="Search">
-            hello
+            <Header currentLocation="San Francisco" />
+            <div className="logo">
+              <img src='/src/assets/logo.png' />
+            </div>
+            <div className="form">
+              <div className="select-date">
+                <div className="cal_img">
+                  <img src='/src/assets/cal2.png'/>
+                </div>
+                <DatePicker 
+                  selected={this.state.date}
+                  onChange={this.handleChange}
+                />
+                <div className="location">
+                  <input
+                    placeholder="Location..."
+                  />
+                </div>
+                <button className="submit">
+                  Search
+                </button>
+              </div>
+            </div>
           </div>
           { webpage }
       </div>
