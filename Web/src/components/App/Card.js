592d7ee45bbfa26d149f02b2d0eb09b493e73afb
John Kim <joimkim@gmail.com>
2017-03-12 12:59:35 -0700

    finish cards

---
+++ Web/src/components/App/Card.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import './card.css';
+
+//resturant schema
+//address: string
+//address2: string
+// city: string
+// country: string
+// dollars: string
+// latitude:
+// longitude:
+// name:
+// options:
+// phone_number:
+// postal_code:
+// profile_url:
+// reservation_url:
+// vibe:
+
+class Card extends React.Component {
+  constructor() {
+    super();
+  
+    this.state = {
+      show: true
+    };
+  }
+
+  render() {
+    const {
+      address,
+      city,
+      dollars,
+      name,
+      phone_number,
+      vibe,
+      options,
+      src
+    } = this.props.rest
+    let Julian;
+    if (name === 'Thirsty Bear') {
+      Julian = src
+    }
+    return (
+      <div className={`Card ${vibe} ${this.state.show ? 'show' : ''}`}>
+        <div className="card-header">
+          <p>{name}</p>
+          <span>{dollars}</span>
+        </div>
+        <div className="card-body-container">
+          <div className="card-img">
+            <img src={ Julian || `https://source.unsplash.com/${src}`}/>
+          </div>
+          <div className="card-body">
+            <div className="vibe">
+              <span><p>{vibe}</p><p>{options}</p></span>
+            </div>
+            <div className="address">
+              <span><p>Address:</p><p>{address}</p></span>
+            </div>
+            <div className="phone_number">
+              <span><p>Phone Number:</p><p>{phone_number}</p></span>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Card;
